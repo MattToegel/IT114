43de612f4009f69674ed54d7c64c35bacd5b9296
MattToegel <matthew.toegel@njit.edu>
2019-09-17 22:04:32 -0400

    Added some class/homework examples

---
+++ Examples/QueueSample.java
@@ -0,0 +1,36 @@
+package com.mt.examples;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class QueueSample {
+	public static void main(String[] args) {
+		Queue<CustomKeyValuePair> queue = new LinkedList<CustomKeyValuePair>();
+		for(int i = 0; i < 10; i++) {
+			queue.add(new CustomKeyValuePair(i, "A Value"));
+		}
+		
+		System.out.println("Show queue: " + queue);
+		
+		CustomKeyValuePair first = queue.remove();
+		System.out.println("Pulled first: " + first);
+		System.out.println("Show altered queue: " + queue);
+		
+		CustomKeyValuePair peek = queue.peek();
+		System.out.println("Just viewing: " + peek);
+		System.out.println("Show unaltered queue: " + queue);
+		
+		
+	}
+}
+class CustomKeyValuePair{
+	public int key;
+	public String value;
+	public CustomKeyValuePair(int k, String v) {
+		this.key = k;
+		this.value = v;
+	}
+	@Override
+	public String toString() {
+		return "{'key':'" + this.key + "', 'value':'" + this.value + "'}";
+	}
+}
