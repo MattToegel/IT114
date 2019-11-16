dd2e84bcb9b0003aa1ea49be87d0aa7c3ec87c93
MattToegel <matthew.toegel@njit.edu>
2019-11-16 12:04:39 -0500

    generate build files and build client/server jars

--- Examples/SocketsPart5/src/MyUI.java
+++ Examples/SocketsPart5/src/MyUI.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.IOException;
 
 import javax.swing.BorderFactory;
