995560e9c7ebffb2e65fd441a4f99c15ca786f6f
MattToegel <matthew.toegel@njit.edu>
2020-02-12 11:51:12 -0500

    Part 1 - Socket Server Sample

---
+++ ServerSocketsSample/src/SampleSocketServer.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class SampleSocketServer {
+	int port = 3002;
+	public SampleSocketServer() {
+	}
+	private void start(int port) {
+		this.port = port;
+		System.out.println("Waiting for client");
+		try (ServerSocket serverSocket = new ServerSocket(port);
+				Socket client = serverSocket.accept();
+				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));) {
+			
+			System.out.println("Client connected, waiting for message");
+			String fromClient = "";
+			String toClient = "";
+			while ((fromClient = in.readLine()) != null) {
+				if ("kill server".equalsIgnoreCase(fromClient)) {
+					System.out.println("Client killed server");
+					break;
+				}
+				else {
+					System.out.println("From client: " + fromClient);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				System.out.println("closing server socket");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+
+	public static void main(String[] arg) {
+		System.out.println("Starting Server");
+		SampleSocketServer server = new SampleSocketServer();
+		server.start(3002);
+		System.out.println("Server Stopped");
+	}
+}
