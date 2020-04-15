1c1b3fa622384787eeb7683c98731a156b3d2375
MattToegel <matthew.toegel@njit.edu>
2020-04-15 14:09:03 -0400

    extracted and renamed Socket Sample Part 2 to new branch

---
+++ SocketsSample_C2S2C/src/SocketClient.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class SocketClient {
+	Socket server;
+	
+	public void connect(String address, int port) {
+		try {
+			server = new Socket(address, port);
+			System.out.println("Client connected");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void start() throws IOException {
+		if(server == null) {
+			return;
+		}
+		System.out.println("Listening for input");
+		try(Scanner si = new Scanner(System.in);
+				PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));){
+			String line = "";
+			while(true) {
+				try {
+					System.out.println("Waiting for input");
+					line = si.nextLine();
+					if(!"quit".equalsIgnoreCase(line)) {
+						out.println(line);
+					}
+					else {
+						break;
+					}
+					line = "";
+					//Added server reply handling in client
+					String fromServer = in.readLine();
+					if(fromServer != null) {
+						System.out.println("Reply from server: " + fromServer);
+					}
+					else {
+						System.out.println("Server disconnected");
+						break;
+					}
+					//end server reply handling in client
+				}
+				catch(Exception e) {
+					System.out.println("Connection dropped");
+					e.printStackTrace();
+					break;
+				}
+			}
+			System.out.println("Exited loop");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			close();
+		}
+	}
+	private void close() {
+		if(server != null) {
+			try {
+				server.close();
+				System.out.println("Closed socket");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) {
+		SocketClient client = new SocketClient();
+		client.connect("127.0.0.1", 3002);
+		try {
+			//if start is private, it's valid here since this main is part of the class
+			client.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
