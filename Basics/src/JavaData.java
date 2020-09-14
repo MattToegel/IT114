84b59d68cbc0cb6bdec06c3e16232ed0c834669d
MattToegel <matthew.toegel@njit.edu>
2020-09-14 13:43:14 -0400

    adding my homework

---
+++ Basics/src/JavaData.java
@@ -0,0 +1,16 @@
+public class JavaData {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		System.out.println("1");
+		// 2. Output the name to the console
+		System.out.println("2");
+		// 3. Create a variable that holds an age (as a whole number)
+		System.out.println("3");
+		// 4. Output the age to the console
+		System.out.println("4");
+		// 5. Create a variable that holds a decimal
+		System.out.println("5");
+		// 6. Output the variable
+	}
+}
