84b61fb66cec060b6dd6a4f59257de1bfcb47bd7
MattToegel <matthew.toegel@njit.edu>
2020-11-18 18:47:15 -0500

    added basics listener for enter key on connection and username page to allow the enter key to move to the next view

--- ProjectPart6/src/client/ClientUI.java
+++ ProjectPart6/src/client/ClientUI.java
@@ -116,6 +116,13 @@ public class ClientUI extends JFrame implements Event {
 	    }
 
 	});
+	// TODO set focus later
+	host.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "sendAction");
+	host.getActionMap().put("sendAction", new AbstractAction() {
+	    public void actionPerformed(ActionEvent actionEvent) {
+		button.doClick();
+	    }
+	});
 	panel.add(button);
 	this.add(panel, "login");
     }
@@ -148,6 +155,13 @@ public class ClientUI extends JFrame implements Event {
 		}
 	    }
 
+	});
+	// TODO set focus later
+	username.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "sendAction");
+	username.getActionMap().put("sendAction", new AbstractAction() {
+	    public void actionPerformed(ActionEvent actionEvent) {
+		button.doClick();
+	    }
 	});
 	panel.add(button);
 	this.add(panel, "details");
@@ -272,6 +286,7 @@ public class ClientUI extends JFrame implements Event {
 
     void addMessage(String str) {
 	JEditorPane entry = new JEditorPane();
+	entry.setContentType("text/html");
 	entry.setEditable(false);
 	// entry.setLayout(null);
 	entry.setText(str);
@@ -328,10 +343,12 @@ public class ClientUI extends JFrame implements Event {
 	    SocketClient.INSTANCE.sendGetRooms(null);
 	    break;
 	default:
+
 	    // no need to react
 	    break;
 	}
 	card.show(this.getContentPane(), panel);
+
     }
 
     void connect(String host, String port) throws IOException {
@@ -438,7 +455,7 @@ public class ClientUI extends JFrame implements Event {
     }
 
     @Override
-    public void onGetTicket(String ticketName, Point position, Point dimension, boolean flag) {
+    public void onGetTicket(String ticketName, Point position, Point dimension, String holder) {
 	// TODO Auto-generated method stub
 
     }
