52aa490570b575e306e7e0a5ad9a7a57de1906e8
MattToegel <matthew.toegel@njit.edu>
2021-04-15 17:49:51 -0400

    fancy text, countdown/timers, ready check

---
+++ Project7/src/client/Event.java
@@ -0,0 +1,25 @@
+package client;
+
+import java.awt.Point;
+
+public interface Event {
+	void onClientConnect(String clientName, String message);
+
+	void onClientDisconnect(String clientName, String message);
+
+	void onMessageReceive(String clientName, String message);
+
+	void onChangeRoom();
+
+	void onSyncDirection(String clientName, Point direction);
+
+	void onSyncPosition(String clientName, Point position);
+
+	void onGetRoom(String roomName);
+
+	void onResize(Point p);
+
+	void onShipPlaced(int shipType, int x, int y, int life);
+
+	void onAttackStatus(int markerType, int x, int y);
+}
