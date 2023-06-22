9f19dc8c9acec0e9c079b78c421da97710f047d0
MattToegel <matthew.toegel@njit.edu>
2023-06-22 15:53:08 -0400

    updated run.sh to pass port to server

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 
+port=3000
+if [ -n "$3" ];
+then
+	port="$3"
+fi
 if [ "$2" = "server" ];
 then
-	java $1.server.Server
+	echo "port $port"
+	java $1.server.Server $port
 elif [ "$2" = "client" ];
 then
 	java $1.client.Client
